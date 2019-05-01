1258b261b9c50de11e8370c1beec5e17d4586ec3
Sallyerik <morillasfj@gmail.com>
2019-05-01 20:26:12 +0200

    Modify authenticate class to include entry arguments validation
    The application needs to identify when the string arguments have been populated in login form, otherwise not email and password are required to login.
    An empty Java String is considered as the not null String that contains zero characters, meaning its length is 0.
    Read more: http://www.java67.com/2014/09/right-way-to-check-if-string-is-empty.html#ixzz5mhNpw7Wp

--- web/app/controllers/Secure.java
+++ web/app/controllers/Secure.java
@@ -17,16 +17,33 @@ public class Secure extends Controller {
         login();
     }
 
-    public static void authenticate(String username, String password){
-        User u = User.loadUser(username);
-        if (u != null && u.getPassword().equals(HashUtils.getMd5(password))){
-            session.put("username", username);
-            session.put("password", password);
-            Application.index();
-        }else{
-            flash.put("error", Messages.get("Public.login.error.credentials"));
-            login();
-        }
-
-    }
+   public static void authenticate(String username, String password){
+    	
+    	//Validate entry arguments username & password
+    	boolean Userempty = StringUtils.isEmpty(username);
+    	boolean Passwordempty = StringUtils.isEmpty(password);
+    	
+    	
+    	 if (Userempty != false ){
+    		 flash.put("error", Messages.get("Public.login.error.email.field"));
+             login();
+         }else{	 
+        	 if (Passwordempty != false ){
+        		 flash.put("error", Messages.get("Public.login.error.password.field"));
+                 login();
+        	 }else{
+		    	System.out.println("username is:" + username);
+		    	User u = User.loadUser(username);
+		        if (u != null && u.getPassword().equals(HashUtils.getMd5(password))){
+		            session.put("username", username);
+		            session.put("password", password);
+		            Application.index();
+		        }else{
+		            flash.put("error", Messages.get("Public.login.error.credentials"));
+		            login();
+		        }
+		
+		    }
+         }
+   }
 }
